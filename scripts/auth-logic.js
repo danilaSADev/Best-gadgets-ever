190d63eed4f3b64478fa142817cfbb2822edd327
danilaSADev <danilas25up@gmail.com>
2021-10-20 20:05:18 +0300

    added logic to registration and sign in form
    - saves data to local storage
    - checks data on sign in

--- scripts/auth-logic.js
+++ scripts/auth-logic.js
@@ -1,9 +1,58 @@
 
-let signInForm = document.getElementById("sign-in")
-let signUpForm = document.getElementById("sign-up")
-
-
-function submitSignInForm() 
+function writeToLocalStorage(data)
 {
-    
+    localStorage.setItem("login", data.email)
+    localStorage.setItem("password", data.password)
+}
+
+function checkWithLocalStorage(data)
+{
+    var isVerified = true
+    if (data.email)
+
+    return false
+}
+
+function getUserData()
+{
+    var data = [localStorage.getItem("login"),  localStorage.getItem("password")]
+    return data
+}
+
+function checkUserInput()
+{
+
+}
+
+function checkLogIn(signInData)
+{
+    var savedData = getUserData()
+    console.log(savedData[0])
+    return savedData[0] == signInData.email && savedData[1] == signInData.password
+}
+
+function getRegistrationData() 
+{
+    var inputField = Array.from(document.getElementById("sign-up").getElementsByTagName("input"))
+    var dataList = []
+    inputField.forEach(element => {
+        dataList[element.name] = element.value
+    });
+    writeToLocalStorage(dataList)
+    console.log(getUserData())
+}
+
+function trySignIn()
+{
+    var signInData = Array.from(document.getElementById("sign-in").getElementsByTagName("input"))
+    var dataList = []
+    signInData.forEach(element => {
+        dataList[element.name] = element.value
+    });
+    if (!checkLogIn(dataList))
+    {
+        alert("Incorrect login and password pair!")
+        return;
+    }
+    window.location.href = "index.html"
 }
