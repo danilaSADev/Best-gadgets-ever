6edf38070443351711306db3cddb0dc0c43671ff
danilaSADev <danilas25up@gmail.com>
2021-10-21 20:53:29 +0300

    added regex check and dynamic header

--- scripts/auth-logic.js
+++ scripts/auth-logic.js
@@ -1,51 +1,40 @@
 sessionStorage.setItem("isLoggedIn", false)
 
+// Validators
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function validatePassword(password)
+{
+    return /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}/g.test(password)
+}
+
+// Data operations
 function writeToLocalStorage(data)
 {
     localStorage.setItem("login", data.email)
     localStorage.setItem("password", data.password)
 }
 
-function checkWithLocalStorage(data)
-{
-    var isVerified = true
-    if (data.email)
-
-    return false
-}
-
-function deleteUserData()
+function clearUserLocalStorage()
 {
     localStorage.clear()
+    sessionStorage.clear()
+    window.location.href = "index.html"
 }
 
-function getUserData()
+function getUserLocalStorage()
 {
     var data = [localStorage.getItem("login"),  localStorage.getItem("password")]
     return data
 }
 
-function checkUserInput()
-{
-    
-}
-
 function checkLogIn(signInData)
 {
-    var savedData = getUserData()
-    console.log(savedData[0])
+    var savedData = getUserLocalStorage()
     return savedData[0] == signInData.email && savedData[1] == signInData.password
-}
-
-function getRegistrationData() 
-{
-    var inputField = Array.from(document.getElementById("sign-up").getElementsByTagName("input"))
-    var dataList = []
-    inputField.forEach(element => {
-        dataList[element.name] = element.value
-    });
-    writeToLocalStorage(dataList)
-    console.log(getUserData())
 }
 
 function trySignIn()
@@ -55,11 +44,37 @@ function trySignIn()
     signInData.forEach(element => {
         dataList[element.name] = element.value
     });
+
     if (!checkLogIn(dataList))
     {
-        alert("Incorrect login and password pair!")
-        return;
+        alert("Incorrect data!")
+        return
     }
-    window.location.href = "index.html"
+
     sessionStorage.setItem("isLoggedIn", true)
+    window.location.href = "index.html"
+}
+
+function tryRegister() 
+{
+    var inputField = Array.from(document.getElementById("sign-up").getElementsByTagName("input"))
+    var dataList = []
+    inputField.forEach(element => {
+        dataList[element.name] = element.value
+    })
+    if (!validateEmail(dataList.email))
+    {
+        alert("Incorrect email!")
+        location.reload()
+        return
+    }
+    if (!validatePassword(dataList.password))
+    {
+        alert("Incorrect password!")
+        location.reload()
+        return
+    }
+    sessionStorage.setItem("isLoggedIn", true)
+    writeToLocalStorage(dataList)
+    window.location.href = "index.html"
 }
--- scripts/utility-loader.js
+++ scripts/utility-loader.js
@@ -2,10 +2,3 @@ $(function(){
     $("header").load("../../templates/utility/header.html"); 
     $("footer").load("../utility/footer.html"); 
 });
-
-function checkIfLoggedIn()
-{
-    console.log(sessionStorage.getItem("isLoggedIn"))
-    document.getElementById("profile-button").hidden = sessionStorage.getItem("isLoggedIn")
-    document.getElementById("auth-buttons").hidden = !sessionStorage.getItem("isLoggedIn")
-}
